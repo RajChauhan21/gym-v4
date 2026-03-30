e9fba73e914909695d3255edada5dc5e4d0ca0a3
Raj Chauhan <craj4757@gmail.com>
2026-03-30 18:53:40 +0530

    added oauth logic

--- src/apis/backend_apis.js
+++ src/apis/backend_apis.js
@@ -1,22 +1,42 @@
-import axios from "axios"
+import axios from "axios";
+import constant from "./constant";
 
-export function getAllOwners(){
-axios.get('http://localhost:8080/gym/findAll',{
-    withCredentials:true
-}).then((response) => {
+export function getAllOwners() {
+  constant.get('/gym/findAll', {
+    withCredentials: true
+  }).then((response) => {
     console.log(response.data)
-})
+  })
 }
 
-export function login(){
-    axios.post(
-  "http://localhost:8080/owner/login",
-  {
-    email: "craj47577@gmail.com",
-    password: "12345"
-  },
-  {
-    withCredentials:true
-  }
-);
+export function login() {
+  constant.post("/owner/login",
+    {
+      email: "manoj@gmail.com",
+      password: "12345"
+    },
+    {
+      withCredentials: true
+    }
+  );
+}
+
+export function saveGym() {
+  constant.post("/gym/save",
+    {
+      name: "Elite Fitness Gym",
+      website: "http://www.elitefitnessgym.com",
+      location: "new panvel, navi mumbai",
+      googleMapUrl: "https://www.google.com/"
+    },
+    {
+      withCredentials: true
+    }
+  ).then((r) => {
+    console.log(r.data)
+  })
+}
+
+export function loginByGoogle() {
+  window.location.href = "http://localhost:8180/oauth2/authorization/google";
 }
---
+++ src/apis/constant.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const constant = axios.create({
+    baseURL: "http://localhost:8180",
+    withCredentials: true
+});
+
+constant.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+
+            originalRequest._retry = true;
+
+            try {
+
+                await axios.post(
+                    "http://localhost:8180/owner/refresh",
+                    {},
+                    { withCredentials: true }
+                );
+
+                return constant(originalRequest);
+
+            } catch (refreshError) {
+
+                console.log("Refresh token expired");
+
+                window.location.href = "http://localhost:8180/oauth2/authorization/google";
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default constant;
